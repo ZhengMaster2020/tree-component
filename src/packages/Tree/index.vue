e6a194c2a51b745abf76f04de00d352bf4a6784c
lizheng <2105195274@qq.com>
2020-05-14 18:48:15 +0800

    init project

---
+++ src/packages/Tree/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tree',
+
+  data() {
+    return {
+    }
+  },
+
+  props: {
+    data: {
+      type: Array
+    }
+  },
+
+  methods: {},
+  created() {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
